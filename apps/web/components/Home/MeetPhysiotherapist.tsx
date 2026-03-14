2ea983b2facff75a20439848f000e8d080c38228
yash-prajapati19 <prajapatiyash194@gmail.com>
2026-03-14 16:30:38 +0530

    Update MeetPhysiotherapist component styles for improved responsiveness; adjust height and positioning of elements to enhance visual consistency across different screen sizes.

--- apps/web/components/Home/MeetPhysiotherapist.tsx
+++ apps/web/components/Home/MeetPhysiotherapist.tsx
@@ -7,7 +7,7 @@ export function MeetPhysiotherapist() {
   return (
     <section id="contact" className="bg-white">
       <div className="py-10 md:py-16">
-          <div className="relative overflow-hidden rounded-bl-xl md:rounded-tl-[180px] rounded-tl-[90px] md:rounded-br-[180px] rounded-br-[90px] md:rounded-tr-2xl rounded-tr-xl bg-primary px-6 py-10 md:px-10 md:py-12">
+          <div className="relative overflow-hidden h-screen md:h-auto rounded-bl-xl md:rounded-tl-[180px] rounded-tl-[90px] md:rounded-br-[180px] rounded-br-[90px] md:rounded-tr-2xl rounded-tr-xl bg-primary px-6 py-10 md:px-10 md:py-12">
             <div className="grid items-center gap-10 md:grid-cols-[1.1fr,0.9fr]">
               {/* Left */}
               <div className="text-white md:pl-20 text-center md:text-left">
@@ -54,9 +54,9 @@ export function MeetPhysiotherapist() {
 
               {/* Right */}
               <FadeIn direction="left" duration={900} distance={60} delay={200}>
-              <div className="relative h-[60vh]">
+              <div className="relative md:h-[60vh] h-[50vh]">
                 {/* Watermark SVG behind doctor */}
-                <div className="pointer-events-none absolute right-5 -top-12 z-5">
+                <div className="pointer-events-none absolute md:right-5 -top-12 -right-10 z-5">
                   <Image
                     src="/white-logo-svg.png"
                     alt=""
@@ -66,7 +66,7 @@ export function MeetPhysiotherapist() {
                   />
                 </div>
 
-<div className="absolute md:-bottom-28 -bottom-16 md:-right-8 right-0 md:w-[40vw] w-full h-full md:h-[600px]">  
+<div className="absolute md:-bottom-28 -bottom-5 md:-right-8 right-0 md:w-[40vw] w-full h-[450px] md:h-[600px]">  
                 <Image
                   src="/doctor.png"
                   alt="Physiotherapist"
